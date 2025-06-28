443f64f102a61a2d1fd34595b1ab346ef25b1588
Timokhinvv <ziro2016z@gmail.com>
2025-06-29 02:27:13 +0300

    main.tf

--- main.tf
+++ main.tf
@@ -7,17 +7,17 @@ terraform {
 }
 
 provider "yandex" {
-  # token     = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  token     = "************************************"
   cloud_id  = "b1gvqhsreifqab3j0l0b"
   folder_id = "b1gn616r8hcg38alqipf"
-  service_account_key_file = file("~/.authorized_key.json")
+  # service_account_key_file = file("~/.authorized_key.json")
 }
 
 
 resource "yandex_compute_instance" "vm-1" {
-  name = "vm-1"
-  hostname = "vm-1"
-  zone = "ru-central1-a"
+  name                      = "vm-1"
+  hostname                  = "vm-1"
+  zone                      = "ru-central1-a"
   allow_stopping_for_update = false
 
   resources {
@@ -28,7 +28,7 @@ resource "yandex_compute_instance" "vm-1" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd89dboump1l490q0osc"
+      image_id = "fd83u9thmahrv9lgedrk"
       size     = 10
     }
   }
@@ -36,17 +36,17 @@ resource "yandex_compute_instance" "vm-1" {
   network_interface {
     subnet_id = yandex_vpc_subnet.bastion-internal-segment.id
     security_group_ids = [
-                           yandex_vpc_security_group.internal-ssh-sg.id,
-                           yandex_vpc_security_group.alb-vm-sg.id,
-                           yandex_vpc_security_group.zabbix-sg.id,
-                           yandex_vpc_security_group.egress-sg.id
-                         ]
-/*    security_group_ids = [
+      yandex_vpc_security_group.internal-ssh-sg.id,
+      yandex_vpc_security_group.alb-vm-sg.id,
+      yandex_vpc_security_group.zabbix-sg.id,
+      yandex_vpc_security_group.egress-sg.id
+    ]
+    /*    security_group_ids = [
                             yandex_vpc_security_group.external-ssh-sg.id,
                             yandex_vpc_security_group.internal-ssh-sg.id
                            ] */
 
-    nat       = false
+    nat        = false
     ip_address = "192.168.10.10"
   }
 
@@ -63,9 +63,9 @@ resource "yandex_compute_instance" "vm-1" {
 
 
 resource "yandex_compute_instance" "vm-2" {
-  name = "vm-2"
-  hostname = "vm-2"
-  zone = "ru-central1-a"
+  name                      = "vm-2"
+  hostname                  = "vm-2"
+  zone                      = "ru-central1-a"
   allow_stopping_for_update = false
 
   resources {
@@ -76,7 +76,7 @@ resource "yandex_compute_instance" "vm-2" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd89dboump1l490q0osc"
+      image_id = "fd83u9thmahrv9lgedrk"
       size     = 10
     }
   }
@@ -84,17 +84,17 @@ resource "yandex_compute_instance" "vm-2" {
   network_interface {
     subnet_id = yandex_vpc_subnet.bastion-internal-segment.id
     security_group_ids = [
-                           yandex_vpc_security_group.internal-ssh-sg.id,
-                           yandex_vpc_security_group.alb-vm-sg.id,
-                           yandex_vpc_security_group.zabbix-sg.id,
-                           yandex_vpc_security_group.egress-sg.id
-                         ]
+      yandex_vpc_security_group.internal-ssh-sg.id,
+      yandex_vpc_security_group.alb-vm-sg.id,
+      yandex_vpc_security_group.zabbix-sg.id,
+      yandex_vpc_security_group.egress-sg.id
+    ]
 
-/*    security_group_ids = [
+    /*    security_group_ids = [
                             yandex_vpc_security_group.external-ssh-sg.id,
                             yandex_vpc_security_group.internal-ssh-sg.id
                            ] */
-    nat       = false
+    nat        = false
     ip_address = "192.168.10.20"
   }
 
@@ -102,7 +102,7 @@ resource "yandex_compute_instance" "vm-2" {
     user-data = "${file("./meta.yml")}"
   }
 
-    scheduling_policy {
+  scheduling_policy {
     preemptible = false
   }
 }
